fdffa52b698c4ac68a80e51a788bfae2524d33e1
rtakeshi13 <rtakeshi13@gmail.com>
2020-05-15 14:12:30 -0300

    mudei as border

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -11,31 +11,28 @@ const MainLayout = styled.div`
   width: 50vw;
   height: 90vh;
   grid-template-columns: 20% 1fr 10%;
-  grid-template-rows: 1fr 25px;
+  grid-template-rows: 1fr 30px;
   grid-template-areas:
   "lista lista lista"
   "user  msg   enviar";
 `
 
 const ListaMensagens = styled.div`
-  border: 1px solid red;
+  border: 2px solid black;
   grid-area: lista;
 `
 
 const InputUser = styled.input`
-  border: 1px solid black;
   padding-left: 5px;
   grid-area: user;
 `
 
 const InputMsg = styled.input`
-  border: 1px solid blue;
   padding-left: 5px;
   grid-area: msg;
 `
 
 const BotaoEnviar = styled.button`
-  border: 1px solid green;
   grid-area: enviar;
 `
 
