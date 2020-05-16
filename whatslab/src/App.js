49041b24305d88242a2c80063b30869532e115c8
Eros Pinder Valente <eros.pv@gmail.com>
2020-05-16 12:51:06 -0300

    desafio 3 finalizado

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import styled from "styled-components";
+import background from './img/space.jpg';
 import Mensagem from "./components/Mensagem/Mensagem";
 
 const MainLayout = styled.div`
   display: grid;
   margin: auto;
   border: 1px solid black;
+  background-image: url(${background});
+  background-size: no-repeat;
   background-color: #e5ddd5;
-  width: 40vw;
+  width: 45vw;
   height: 90vh;
   padding-bottom: 5px;
   grid-column-gap: 10px;
@@ -22,6 +25,9 @@ const MainLayout = styled.div`
 const ListaMensagens = styled.div`
   padding: 20px;
   grid-area: lista;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
 `
 
 const InputUser = styled.input`
@@ -104,7 +110,7 @@ class App extends React.Component {
       <MainLayout>
         <ListaMensagens>{listaMensgens}</ListaMensagens>
         <InputUser placeholder='Usuário' value={this.state.valorInputUsuario} onChange={this.onChangeInputUsuario}/>
-        <InputMsg placeholder='Mensagem' value={this.state.valorInputMensagem} onChange={this.onChangeInputMensagem}/>
+        <InputMsg placeholder='Mensagem' value={this.state.valorInputMensagem} onChange={this.onChangeInputMensagem} onKeyPress={this.onKeyPressInputMensagem}/>
         <BotaoEnviar onClick={this.enviarMensagem}>Enviar</BotaoEnviar>
       </MainLayout>
     );
--- whatslab/src/components/Mensagem/Mensagem.js
+++ whatslab/src/components/Mensagem/Mensagem.js
@@ -2,10 +2,51 @@ import React from 'react';
 import styled from "styled-components";
 import "./Mensagem.css";
 
+const MensagemEsquerda = styled.div`
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    margin: 10px 0 10px;
+    padding: 10px;
+    max-width: 40%;
+    border-radius: 5px;
+    box-shadow: 0 1px 1px rgb(0, 0, 0, 0.4);
+    align-self: flex-start;
+`
+const MensagemDireita = styled.div`
+    background-color: #dcf8c6;
+    display: flex;
+    flex-direction: column;
+    margin: 10px 0 10px;
+    padding: 10px;
+    max-width: 40%;
+    border-radius: 5px;
+    box-shadow: 0 1px 1px rgb(0, 0, 0, 0.4);
+    align-self: flex-end;
+`
+
+const NomeUsuario = styled.p`
+    font-weight: bold;
+    margin-bottom: 8px;
+`
+const MensagemUsuario = styled.p`
+    width:100%;
+    word-wrap: break-word;
+`
+
 
 class Mensagem extends React.Component{
-    render(){
-    return <p onDoubleClick={this.props.funcaoDeletar}><b>{this.props.nomeUsuario}</b>: {this.props.valorMensagem}</p>
+    render() {
+        if (this.props.nomeUsuario === "eu") {
+            return <MensagemDireita onDoubleClick={this.props.funcaoDeletar}>
+                <MensagemUsuario>{this.props.valorMensagem}</MensagemUsuario>
+            </MensagemDireita>
+        } else {
+            return <MensagemEsquerda onDoubleClick={this.props.funcaoDeletar}>
+                <NomeUsuario>{this.props.nomeUsuario}</NomeUsuario>
+                <MensagemUsuario>{this.props.valorMensagem}</MensagemUsuario>
+            </MensagemEsquerda>
+        }            
     }
 }
 
